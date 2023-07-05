c4540fcf564cc126dc6b439b429f142668415510
Tomasz Zieliński <tzn@touk.pl>
2023-07-05 15:54:49 +0200

    qs

--- src/bricks/brick.rs
+++ src/bricks/brick.rs
@@ -2,25 +2,24 @@ pub mod brick {
   use std::iter::Map;
   use serde::{Deserialize, Serialize};
 
-  pub trait Param<'de>: Serialize + Deserialize<'de> {}
+  pub trait Param: Serialize + Deserialize<'static> {}
 
   // consider https://github.com/rust-phf/rust-phf for SplitterBrick
-  pub enum Brick<'de, Split, const ConsumesSize: usize>
-    where Split: Sized
+  pub enum Brick<Split: Param>
   {
-    LinearBrick { name: String, consumes: [Box<dyn Param<'de>>; ConsumesSize], brick_type: BrickType },
-    SplitterBrick { name: String, consumes: Map<Split, Box<dyn Param<'de>>>, brick_type: BrickType },
-  }
-
-
-  // mix it into brick enum????
-  pub enum BrickType {
-    FollowingBrick,
-    FinalBrick(FinalBrick),
-  }
-
-  pub enum FinalBrick {
-    ResultBrickType,
-    RedirectionBrickType,
+    LinearBrick {
+      name: String,
+      consumes: Vec<Box<dyn Param>>,
+      produces: Vec<Box<dyn Param>>,
+    },
+    FinalBrick {
+      name: String,
+      consumes: Vec<Box<dyn Param>>,
+    },
+    SplitterBrick {
+      name: String,
+      consumes: Vec<Box<dyn Param>>,
+      produces: Map<Split, Vec<Box<dyn Param>>>,
+    },
   }
 }
