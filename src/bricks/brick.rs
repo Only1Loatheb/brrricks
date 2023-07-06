01522013ddd0c2dfaa4ff7339951139699c7d4fc
Tomasz Zieliński <tzn@touk.pl>
2023-07-06 19:02:00 +0200

    qs

--- src/bricks/brick.rs
+++ src/bricks/brick.rs
@@ -1,25 +1,36 @@
 pub mod brick {
   use std::iter::Map;
-  use serde::{Deserialize, Serialize};
 
-  pub trait Param: Serialize + Deserialize<'static> {}
+  pub struct ParamSerializationError;
+
+  pub struct ParamDeserializationError;
+
+  pub trait Param {
+    type T;
+
+    fn name(&self) -> String;
+
+    fn serialize(&self, t: Self::T) -> Result<String, ParamSerializationError>;
+
+    fn deserialize(&self, serialized: &str) -> Result<Self::T, ParamDeserializationError>;
+  }
 
   // consider https://github.com/rust-phf/rust-phf for SplitterBrick
-  pub enum Brick<Split: Param>
+  pub enum Brick<AppParams, Split: Param>
   {
     LinearBrick {
       name: String,
-      consumes: Vec<Box<dyn Param>>,
-      produces: Vec<Box<dyn Param>>,
+      consumes: Vec<Box<dyn Param<T=AppParams>>>,
+      produces: Vec<Box<dyn Param<T=AppParams>>>,
     },
     FinalBrick {
       name: String,
-      consumes: Vec<Box<dyn Param>>,
+      consumes: Vec<Box<dyn Param<T=AppParams>>>,
     },
     SplitterBrick {
       name: String,
-      consumes: Vec<Box<dyn Param>>,
-      produces: Map<Split, Vec<Box<dyn Param>>>,
+      consumes: Vec<Box<dyn Param<T=AppParams>>>,
+      produces: Map<Split, Vec<Box<dyn Param<T=AppParams>>>>,
     },
   }
 }
