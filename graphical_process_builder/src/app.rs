adb70a86bbfc0209a0e84778d801576320d2b504
Tomasz Zieliński <tzn@touk.pl>
2024-03-17 13:04:17 +0100

    qs

--- graphical_process_builder/src/app.rs
+++ graphical_process_builder/src/app.rs
@@ -1,11 +1,13 @@
-use std::ops::Not;
+use std::cmp::max_by;
+use std::ops::{Deref, Not};
 use eframe::emath::Pos2;
 use eframe::epaint::{Color32, Rect, Shape};
-use egui::{Key, Label, Order, PointerButton, Stroke, TextEdit, Ui, Vec2, Widget};
+use egui::{Context, Key, Label, Order, PointerButton, Response, Stroke, TextEdit, Ui, Vec2, Widget};
+use egui::Key::M;
 use crate::brick::*;
 
 #[derive(serde::Deserialize, serde::Serialize)]
-struct BrickRect {
+struct BrickModel {
   name: String,
   rect: Rect,
   brick: Brick,
@@ -18,11 +20,12 @@ const BRICK_INIT_SIZE: Vec2 = Vec2 { x: 100.0, y: 100.0 };
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-  bricks: Vec<BrickRect>,
+  bricks: Vec<BrickModel>,
   edges: Vec<(usize, usize)>,
-  #[serde(skip)] // This how you opt-out of serialization of a field
-  edge_start: Option<usize>,
+  #[serde(skip)]
   next_brick_uuid: usize,
+  #[serde(skip)]
+  edge_start: Option<(Pos2, usize)>,
 }
 
 impl Default for TemplateApp {
@@ -30,11 +33,14 @@ impl Default for TemplateApp {
     Self {
       bricks: vec![],
       edges: vec![],
-      edge_start: None,
       next_brick_uuid: 0,
+      edge_start: None,
     }
   }
 }
+
+static line: Stroke = Stroke { width: 1.0, color: Color32::LIGHT_BLUE };
+const edge_draw_button: PointerButton = PointerButton::Primary;
 
 impl TemplateApp {
   /// Called once before the first frame.
@@ -44,109 +50,140 @@ impl TemplateApp {
 
     // Load previous app state (if any).
     // Note that you must enable the `persistence` feature for this to work.
-    // if let Some(storage) = cc.storage {
-    //   return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-    // }
+    if let Some(storage) = cc.storage {
+      let mut app: TemplateApp = eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+      app.next_brick_uuid = app.bricks.iter().map(|brick| brick.uuid).max_by(|left, right| left.cmp(&right)).unwrap_or(0) + 1;
+      return app;
+    }
 
     Default::default()
   }
+
+  fn edges(&mut self, ui: &Ui) {
+    ui.painter().extend(self.edges.iter().filter_map(|edge| {
+      if let (Some(a), Some(b)) = (self.bricks.iter().find(|brick| brick.uuid == edge.0), self.bricks.iter().find(|brick| brick.uuid == edge.1)) {
+        Some(Shape::LineSegment {
+          points: [a.rect.center_bottom(), b.rect.center_top()],
+          stroke: line,
+        })
+      } else { None }
+    }));
+  }
+
+  fn bricks(&mut self, ctx: &Context, ui: &mut Ui) {
+    if let (true, Some(mouse), Some(edge_start)) = (ctx.input(|i| { i.pointer.primary_released() }), ctx.pointer_latest_pos(), self.edge_start) {
+      let maybe_edge = self.retain_bricks_with_add_edge(ui, |(brick_model, brick_rect)| {
+        TemplateApp::create_edge(mouse, edge_start.1, brick_model, brick_rect)
+      });
+      if let Some(edge) = maybe_edge {
+        self.edges.push(edge);
+      }
+      self.edge_start = None;
+    } else {
+      self.retain_bricks_with_add_edge(ui, |_| { None });
+    }
+  }
+
+  fn retain_bricks_with_add_edge(&mut self, ui: &mut Ui, create_edge: impl Fn((&BrickModel, Rect)) -> Option<(usize, usize)>) -> Option<(usize, usize)> {
+    let mut edge_to_add = None;
+    self.bricks.retain_mut(|brick_model| {
+      let mut keep = true;
+      ui.put(brick_model.rect, |ui: &mut Ui| {
+        let brick_script_editor = ui.code_editor(&mut brick_model.name);
+        keep = brick_script_editor.clicked_by(PointerButton::Secondary).not();
+        if brick_script_editor.drag_started_by(edge_draw_button) {
+          println!("start edge");
+          self.edge_start = Some((brick_model.rect.center_bottom(), brick_model.uuid));
+        }
+        let maybe_created_edge = create_edge((&brick_model, brick_script_editor.rect));
+        edge_to_add = edge_to_add.or(maybe_created_edge);
+        brick_script_editor
+      },
+      );
+      if keep.not() {
+        self.edges.retain(|edge| edge.0 != brick_model.uuid && edge.1 != brick_model.uuid)
+      }
+      keep
+    }
+    );
+    edge_to_add
+  }
+
+  fn create_edge(mouse: Pos2, edge_start: usize, brick_model: &BrickModel, brick_rect: Rect) -> Option<(usize, usize)> {
+    if brick_rect.contains(mouse) && edge_start != brick_model.uuid {
+      dbg!("draw", edge_start, brick_model.uuid);
+      Some((edge_start, brick_model.uuid))
+    } else { None }
+  }
+
+
+  fn show_edge_creation(&mut self, ctx: &Context, ui: &Ui) {
+    if let (Some(edge_start), Some(mouse)) = (self.edge_start, ctx.pointer_latest_pos()) {
+      ui.painter().add(
+        Shape::LineSegment {
+          points: [edge_start.0, mouse],
+          stroke: line,
+        });
+    }
+  }
+
+  fn handle_brick_creation(&mut self, ctx: &Context) {
+    if ctx.is_context_menu_open().not()
+      && ctx.wants_keyboard_input().not()
+      && ctx.input(|i| i.pointer.primary_pressed()) {
+      match ctx.pointer_interact_pos() {
+        Some(pos) if ctx.layer_id_at(pos).is_some_and(|x| x.order != Order::Background) => {
+          println!("over something")
+        }
+        Some(pos) => {
+          println!("create brick");
+          self.bricks.push(
+            BrickModel {
+              name: "aaa".to_owned(),
+              rect: Rect::from_center_size(pos, BRICK_INIT_SIZE),
+              brick: Default::default(),
+              uuid: self.next_brick_uuid,
+            });
+          self.next_brick_uuid += 1;
+        }
+        None => {
+          println!("not create brick")
+        }
+      }
+    }
+  }
+
+  fn menu(&mut self, ctx: &Context, ui: &mut Ui) {
+    ui.menu_button("File", |ui| {
+      if ui.button("Quit").clicked() {
+        ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+      }
+    });
+    ui.menu_button("Clean", |ui| {
+      if ui.button("I am sure").clicked() {
+        self.bricks.clear();
+        self.edges.clear();
+      }
+    });
+    ui.add_space(16.0);
+  }
 }
 
-static line: Stroke = Stroke { width: 10.0, color: Color32::LIGHT_BLUE };
-const edge_draw_button: PointerButton = PointerButton::Primary;
-
 impl eframe::App for TemplateApp {
-  /// Called by the frame work to save state before shutdown.
   fn save(&mut self, storage: &mut dyn eframe::Storage) {
     eframe::set_value(storage, eframe::APP_KEY, self);
   }
-
-  /// Called each time the UI needs repainting, which may be many times per second.
-  fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-    // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-    // For inspiration and more examples, go to https://emilk.github.io/egui
-
+  fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
     egui::CentralPanel::default().show(ctx, |ui| {
-      self.bricks.retain_mut(|b|
-        {
-          let mut keep = true;
-          ui.put(b.rect, |ui: &mut Ui| {
-            let brick_script_editor = ui.code_editor(&mut b.name);
-            keep = brick_script_editor.clicked_by(PointerButton::Secondary).not();
-            brick_script_editor
-          },
-          );
-          keep
-        }
-      );
-      ui.painter().add(Shape::Vec(self.edges.iter().map(|x| {
-        Shape::LineSegment {
-          points: [self.bricks[x.0].rect.center_bottom(), self.bricks[x.1].rect.center_top()],
-          stroke: line,
-        }
-      }).collect()));
-
-      // if ctx.drag_started_by(edge_draw_button) {
-      //   println!("start edge");
-      //   self.edge_start = Some(b.uuid);
-      // }
-      // if brick_script_editor.drag_released_by(edge_draw_button) {
-      //   match self.edge_start {
-      //     Some(uuid) if uuid != b.uuid => {
-      //       println!("draw edge");
-      //       self.edges.push((uuid, b.uuid));
-      //     }
-      //     _ => {
-      //       println!("not draw edge");
-      //     }
-      //   }
-      //   self.edge_start = None;
-      // }
-      if ctx.is_context_menu_open().not()
-        && ctx.wants_keyboard_input().not()
-        && ctx.input(|i| i.pointer.primary_pressed()) {
-        match ctx.pointer_interact_pos() {
-          Some(pos) if ctx.layer_id_at(pos).is_some_and(|x|x.order != Order::Background) => {
-            println!("over something")
-          }
-          Some(pos) => {
-            println!("create brick");
-            self.bricks.push(
-              BrickRect {
-                name: "aaa".to_owned(),
-                rect: Rect::from_center_size(pos, BRICK_INIT_SIZE),
-                brick: Default::default(),
-                uuid: self.next_brick_uuid,
-              });
-            self.next_brick_uuid += 1;
-          }
-          None => {
-            println!("not create brick")
-          }
-        }
-      }
+      self.bricks(ctx, ui);
+      self.edges(ui);
+      self.show_edge_creation(ctx, ui);
+      self.handle_brick_creation(ctx);
     });
     egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-      // The top panel is often a good place for a menu bar:
-
       egui::menu::bar(ui, |ui| {
-        // NOTE: no File->Quit on web pages!
         let is_web = cfg!(target_arch = "wasm32");
-        if !is_web {
-          ui.menu_button("File", |ui| {
-            if ui.button("Quit").clicked() {
-              ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-            }
-          });
-          ui.menu_button("Clean", |ui| {
-            if ui.button("I am sure").clicked() {
-              self.bricks.clear();
-              self.edges.clear();
-            }
-          });
-          ui.add_space(16.0);
-        }
-
+        if !is_web { self.menu(ctx, ui); }
         egui::widgets::global_dark_light_mode_buttons(ui);
       });
     });
