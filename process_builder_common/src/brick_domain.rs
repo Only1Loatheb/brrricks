b4876aed7794e8f940aa6418cded3d1160a3841e
Tomasz Zieliński <zielinski.x360@gmail.com>
2024-09-22 12:44:48 +0200

    qs

--- process_builder_common/src/brick_domain.rs
+++ process_builder_common/src/brick_domain.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use async_trait::async_trait;
+use serde::Serializer;
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[derive(PartialEq, Debug, Eq, Clone, Copy, PartialOrd, Ord, Hash)]
@@ -9,7 +10,6 @@ pub struct ParamId(pub usize);
 #[derive(PartialEq, Debug, Eq, Clone, Copy, PartialOrd, Ord, Hash)]
 pub struct ActionId(pub usize);
 
-#[derive(serde::Deserialize, serde::Serialize)]
 #[derive(PartialEq, Debug, Eq, Clone, PartialOrd, Ord, Hash)]
 pub struct Message(pub String);
 
@@ -17,35 +17,31 @@ pub struct Message(pub String);
 #[derive(PartialEq, Debug, Eq, Clone, Copy, PartialOrd, Ord, Hash)]
 pub struct SplitIndex(pub usize);
 
-#[derive(serde::Deserialize, serde::Serialize)]
 #[derive(PartialEq, Debug, Eq, Clone)]
 pub struct InputParams(pub HashMap<ParamId, serde_json::value::Value>);
 
-#[derive(serde::Deserialize, serde::Serialize)]
 #[derive(PartialEq, Debug, Eq, Clone)]
 pub struct OutputParams(pub HashMap<ParamId, serde_json::value::Value>);
 
 // process breaking requires explicit split with final brick
-#[derive(serde::Deserialize, serde::Serialize)]
 #[derive(PartialEq, Debug, Eq, Clone)]
 pub struct LinearOutput(pub Option<Message>, pub OutputParams);
 
 // process breaking requires explicit split with final brick
-#[derive(serde::Deserialize, serde::Serialize)]
 #[derive(PartialEq, Debug, Eq, Clone)]
 pub struct SplitterOutput(pub SplitIndex, pub OutputParams);
 
 #[async_trait]
-pub trait LinearBrickHandler {
-  async fn handle(&self, input: InputParams) -> anyhow::Result<LinearOutput>;
+pub trait LinearBrickHandler<SERIALIZER: Serializer> {
+  async fn handle(&self, input: InputParams, serializer: SERIALIZER) -> anyhow::Result<LinearOutput>;
 }
 
 #[async_trait]
-pub trait SplitterBrickHandler {
-  async fn handle(&self, input: InputParams) -> anyhow::Result<SplitterOutput>;
+pub trait SplitterBrickHandler<SERIALIZER: Serializer> {
+  async fn handle(&self, input: InputParams, serializer: SERIALIZER) -> anyhow::Result<SplitterOutput>;
 }
 
 #[async_trait]
-pub trait FinalBrickHandler {
-  async fn handle(&self, input: InputParams) -> anyhow::Result<Message>;
+pub trait FinalBrickHandler<SERIALIZER: Serializer> {
+  async fn handle(&self, input: InputParams, serializer: SERIALIZER) -> anyhow::Result<Message>;
 }
--- type_process_builder/src/brick.rs
+++ type_process_builder/src/brick.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::marker::PhantomData;
 
 use crate::invariant::Invariant;
@@ -5,29 +6,54 @@ use async_trait::async_trait;
 use frunk_core::coproduct::{CNil, Coproduct};
 use frunk_core::hlist::{HCons, HList, HNil};
 use process_builder_common::brick_domain::*;
-
+use serde::{Serialize, Deserialize, Serializer};
+use serde_json::Value;
 // #[derive(PartialEq, Debug, Eq, Clone, Copy, PartialOrd, Ord, Hash)]
 
-pub struct ParamRepr<'same_process, PARAM_VALUE> {
+pub trait ParamValue<SERIALIZER: Serializer>: Serialize + for<'de> Deserialize<'de> {}
+
+pub trait ParamList<'same_process, SERIALIZER: Serializer>: HList {
+  fn get_params(self, serializer: SERIALIZER) -> Vec<Value>;
+}
+
+impl<SERIALIZER: Serializer> ParamList<SERIALIZER> for HNil {
+  fn get_params(self, _serializer: SERIALIZER) -> Vec<Value> {
+    vec![]
+  }
+}
+
+impl<'same_process, SERIALIZER: Serializer, PARAM_VALUE: ParamValue<SERIALIZER>, TAIL: ParamList<SERIALIZER>>
+ParamList<'same_process, SERIALIZER>
+for HCons<PARAM_VALUE, TAIL> {
+  fn get_params(self, serializer: SERIALIZER) -> Vec<Value> {
+    let mut param_ids = self.tail.get_param_ids();
+    param_ids.push(self.head.serialize(serializer));
+    param_ids
+  }
+}
+
+pub struct ParamRepr<'same_process, SERIALIZER: Serializer, PARAM_VALUE: ParamValue<SERIALIZER>> {
   pub(crate) same_process_invariant: Invariant<'same_process>,
   pub(crate) param_value: PhantomData<PARAM_VALUE>,
   pub(crate) param_id: ParamId,
 }
 
-pub trait ParamReprList<'same_process>: HList {
+pub trait ParamReprList<'same_process, SERIALIZER: Serializer>: HList {
   fn get_param_ids(self) -> Vec<ParamId>;
-  type VALUE: HList;
+  type VALUE: ParamList<'same_process, SERIALIZER>;
 }
 
-impl ParamReprList for HNil {
+impl<'same_process, SERIALIZER: Serializer> ParamReprList<'same_process, SERIALIZER> for HNil {
   fn get_param_ids(self) -> Vec<ParamId> {
     vec![]
   }
+
   type VALUE = HNil;
 }
 
-impl<'same_process, PARAM_VALUE, TAIL: ParamReprList> ParamReprList<'same_process>
-for HCons<ParamRepr<'same_process, PARAM_VALUE>, TAIL> {
+impl<'same_process, SERIALIZER: Serializer, PARAM_VALUE: ParamValue<SERIALIZER>, TAIL: ParamReprList<'same_process, SERIALIZER>>
+ParamReprList<'same_process, SERIALIZER>
+for HCons<ParamRepr<'same_process, SERIALIZER, PARAM_VALUE>, TAIL> {
   fn get_param_ids(self) -> Vec<ParamId> {
     let mut param_ids = self.tail.get_param_ids();
     param_ids.push(self.head.param_id);
@@ -39,17 +65,41 @@ for HCons<ParamRepr<'same_process, PARAM_VALUE>, TAIL> {
 #[async_trait]
 pub trait TypeLinearBrickHandler<
   'same_process,
-  CONSUMES: ParamReprList<'same_process>,
-  PRODUCES: ParamReprList<'same_process>,
+  SERIALIZER: Serializer,
+  CONSUMES: ParamReprList<'same_process, SERIALIZER>,
+  PRODUCES: ParamReprList<'same_process, SERIALIZER>,
 > {
-  async fn handle(&self, input: CONSUMES::VALUE) -> anyhow::Result<PRODUCES::VALUE>;
+  async fn handle(&self, input: CONSUMES::VALUE) -> anyhow::Result<(Option<Message>, PRODUCES::VALUE)>;
+
+  fn produces() -> PRODUCES;
+}
+
+#[async_trait]
+impl<
+  'same_process,
+    SERIALIZER: Serializer,
+  CONSUMES: ParamReprList<'same_process, SERIALIZER>,
+  PRODUCES: ParamReprList<'same_process, SERIALIZER>,
+> LinearBrickHandler<SERIALIZER>
+for dyn TypeLinearBrickHandler<'same_process, SERIALIZER, CONSUMES, PRODUCES> {
+  async fn handle(&self, input: InputParams, serializer: SERIALIZER) -> anyhow::Result<LinearOutput> {
+    let result = self.handle(input).await?;
+    let param_ids:Vec<ParamId> =  vec![];
+    let param_values = result.1.get_params(serializer);
+    anyhow::Ok(LinearOutput(result.0, OutputParams(param_ids.iter().zip(param_values).into())))
+  }
 }
 
 /// We can add a list of completed actions later
-pub struct LinearBrick<'same_process, CONSUMES: ParamReprList<'same_process>, PRODUCES: ParamReprList<'same_process>> {
+pub struct LinearBrick<
+  'same_process,
+  SERIALIZER: Serializer,
+  CONSUMES: ParamReprList<'same_process, SERIALIZER>,
+  PRODUCES: ParamReprList<'same_process, SERIALIZER>,
+> {
   pub name: String,
   pub produces: PRODUCES,
-  pub handler: Box<dyn TypeLinearBrickHandler<'same_process, CONSUMES, PRODUCES>>,
+  pub handler: Box<dyn TypeLinearBrickHandler<'same_process, SERIALIZER, CONSUMES, PRODUCES>>,
 }
 
 /// Let's hope rust will optimise frunk::coproduct::Coproduct just fine :copium:
@@ -58,7 +108,11 @@ pub trait SplitterOutputRepr<'same_process> {
   fn index(self) -> usize;
 }
 
-impl<'same_process, CASE_THIS: ParamReprList<'same_process>> SplitterOutputRepr<'same_process> for Coproduct<CASE_THIS, CNil> {
+impl<
+  'same_process,
+  SERIALIZER: Serializer,
+  CASE_THIS: ParamReprList<'same_process, SERIALIZER>,
+> SplitterOutputRepr<'same_process> for Coproduct<CASE_THIS, CNil> {
   fn get_param_ids(self) -> Vec<ParamId> {
     match self {
       Coproduct::Inl(inl) => inl.get_param_ids(),
@@ -74,7 +128,12 @@ impl<'same_process, CASE_THIS: ParamReprList<'same_process>> SplitterOutputRepr<
   }
 }
 
-impl<'same_process, CASE_THIS: ParamReprList<'same_process>, CASE_OTHER: SplitterOutputRepr<'same_process>> SplitterOutputRepr<'same_process> for Coproduct<CASE_THIS, CASE_OTHER> {
+impl<
+  'same_process,
+  SERIALIZER: Serializer,
+  CASE_THIS: ParamReprList<'same_process, SERIALIZER>,
+  CASE_OTHER: SplitterOutputRepr<'same_process>,
+> SplitterOutputRepr<'same_process> for Coproduct<CASE_THIS, CASE_OTHER> {
   fn get_param_ids(self) -> Vec<ParamId> {
     match self {
       Coproduct::Inl(inl) => inl.get_param_ids(),
