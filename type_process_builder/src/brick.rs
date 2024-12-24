67cd0bafc50e665cf0f5498950051e496fa30199
Tomasz Zieliński <zielinski.x360@gmail.com>
2024-12-24 13:05:46 +0100

    simplify stuff a bit

--- type_process_builder/src/brick.rs
+++ type_process_builder/src/brick.rs
@@ -11,75 +11,37 @@ use serde::{Deserialize, Serialize, Serializer};
 
 pub trait ParamValue: Serialize + for<'de> Deserialize<'de> {}
 
-pub trait ParamList<'same_process>: HList {
-    fn get_params(self) -> Vec<Value>;
+pub trait ParamList: HList {}
+
+impl ParamList for HNil {}
+
+impl<PARAM_VALUE: ParamValue, TAIL: ParamList> ParamList for HCons<PARAM_VALUE, TAIL> {}
+
+pub struct ParamRepr<'same_process, PARAM_VALUE: ParamValue> {
+  pub(crate) same_process_invariant: Invariant<'same_process>,
+  pub(crate) param_value: PhantomData<PARAM_VALUE>,
+  pub(crate) param_id: ParamId,
 }
 
-impl ParamList for HNil {
-    fn get_params(self, _serializer: SERIALIZER) -> Vec<Value> {
-        vec![]
-    }
+pub trait ParamReprList: HList {
+  type VALUE: ParamList;
 }
 
-impl<'same_process, SERIALIZER: Serializer, PARAM_VALUE: ParamValue, TAIL: ParamList>
-    ParamList<'same_process, SERIALIZER> for HCons<PARAM_VALUE, TAIL>
+impl<'same_process, SERIALIZER: Serializer> ParamReprList for HNil {
+  type VALUE = HNil;
+}
+
+impl<'same_process, PARAM_VALUE: ParamValue, TAIL: ParamReprList> ParamReprList
+  for HCons<ParamRepr<'same_process, PARAM_VALUE>, TAIL>
 {
-    fn get_params(self) -> Vec<Value> {
-        let mut param_ids = self.tail.get_param_ids();
-        param_ids.push(self.head.serialize(serializer));
-        param_ids
-    }
-}
-
-pub struct ParamRepr<'same_process, SERIALIZER: Serializer, PARAM_VALUE: ParamValue> {
-    pub(crate) same_process_invariant: Invariant<'same_process>,
-    pub(crate) param_value: PhantomData<PARAM_VALUE>,
-    pub(crate) param_id: ParamId,
-}
-
-pub trait ParamReprList<'same_process, SERIALIZER: Serializer>: HList {
-    fn get_param_ids(self) -> Vec<ParamId>;
-    type VALUE: ParamList<'same_process, SERIALIZER>;
-}
-
-impl<'same_process, SERIALIZER: Serializer> ParamReprList<'same_process, SERIALIZER> for HNil {
-    fn get_param_ids(self) -> Vec<ParamId> {
-        vec![]
-    }
-
-    type VALUE = HNil;
-}
-
-impl<
-        'same_process,
-        SERIALIZER: Serializer,
-        PARAM_VALUE: ParamValue,
-        TAIL: ParamReprList<'same_process, SERIALIZER>,
-    > ParamReprList<'same_process, SERIALIZER>
-    for HCons<ParamRepr<'same_process, SERIALIZER, PARAM_VALUE>, TAIL>
-{
-    fn get_param_ids(self) -> Vec<ParamId> {
-        let mut param_ids = self.tail.get_param_ids();
-        param_ids.push(self.head.param_id);
-        param_ids
-    }
-    type VALUE = HCons<PARAM_VALUE, TAIL::VALUE>;
+  type VALUE = HCons<PARAM_VALUE, TAIL::VALUE>;
 }
 
 #[async_trait]
-pub trait TypeLinearBrickHandler<
-    'same_process,
-    SERIALIZER: Serializer,
-    CONSUMES: ParamReprList<'same_process, SERIALIZER>,
-    PRODUCES: ParamReprList<'same_process, SERIALIZER>,
->
-{
-    async fn handle(
-        &self,
-        input: CONSUMES::VALUE,
-    ) -> anyhow::Result<(Option<Message>, PRODUCES::VALUE)>;
+pub trait TypeLinearBrickHandler<'same_process, CONSUMES: ParamReprList, PRODUCES: ParamReprList> {
+  async fn handle(&self, input: CONSUMES::VALUE) -> anyhow::Result<(Option<Message>, PRODUCES::VALUE)>;
 
-    fn produces() -> PRODUCES;
+  // fn produces() -> PRODUCES;
 }
 
 // #[async_trait]
@@ -104,73 +66,66 @@ pub trait TypeLinearBrickHandler<
 
 /// We can add a list of completed actions later
 pub struct LinearBrick<
-    'same_process,
-    SERIALIZER: Serializer,
-    CONSUMES: ParamReprList<'same_process, SERIALIZER>,
-    PRODUCES: ParamReprList<'same_process, SERIALIZER>,
+  'same_process,
+  SERIALIZER: Serializer,
+  CONSUMES: ParamReprList<'same_process, SERIALIZER>,
+  PRODUCES: ParamReprList<'same_process, SERIALIZER>,
 > {
-    pub name: String,
-    pub produces: PRODUCES,
-    pub handler: Box<dyn TypeLinearBrickHandler<'same_process, SERIALIZER, CONSUMES, PRODUCES>>,
+  pub name: String,
+  pub produces: PRODUCES,
+  pub handler: Box<dyn TypeLinearBrickHandler<'same_process, SERIALIZER, CONSUMES, PRODUCES>>,
 }
 
 /// Let's hope rust will optimise frunk::coproduct::Coproduct just fine :copium:
 pub trait SplitterOutputRepr<'same_process> {
-    fn get_param_ids(self) -> Vec<ParamId>;
-    fn index(self) -> usize;
+  fn get_param_ids(self) -> Vec<ParamId>;
+  fn index(self) -> usize;
+}
+
+impl<'same_process, SERIALIZER: Serializer, CASE_THIS: ParamReprList<'same_process, SERIALIZER>>
+  SplitterOutputRepr<'same_process> for Coproduct<CASE_THIS, CNil>
+{
+  fn get_param_ids(self) -> Vec<ParamId> {
+    match self {
+      Coproduct::Inl(inl) => inl.get_param_ids(),
+      Coproduct::Inr(cnil) => match cnil {},
+    }
+  }
+
+  fn index(self) -> usize {
+    match self {
+      Coproduct::Inl(_) => 0,
+      Coproduct::Inr(cnil) => match cnil {},
+    }
+  }
 }
 
 impl<
-        'same_process,
-        SERIALIZER: Serializer,
-        CASE_THIS: ParamReprList<'same_process, SERIALIZER>,
-    > SplitterOutputRepr<'same_process> for Coproduct<CASE_THIS, CNil>
+    'same_process,
+    SERIALIZER: Serializer,
+    CASE_THIS: ParamReprList<'same_process, SERIALIZER>,
+    CASE_OTHER: SplitterOutputRepr<'same_process>,
+  > SplitterOutputRepr<'same_process> for Coproduct<CASE_THIS, CASE_OTHER>
 {
-    fn get_param_ids(self) -> Vec<ParamId> {
-        match self {
-            Coproduct::Inl(inl) => inl.get_param_ids(),
-            Coproduct::Inr(cnil) => match cnil {},
-        }
+  fn get_param_ids(self) -> Vec<ParamId> {
+    match self {
+      Coproduct::Inl(inl) => inl.get_param_ids(),
+      Coproduct::Inr(inr) => inr.get_param_ids(),
     }
+  }
 
-    fn index(self) -> usize {
-        match self {
-            Coproduct::Inl(_) => 0,
-            Coproduct::Inr(cnil) => match cnil {},
-        }
+  fn index(self) -> usize {
+    match self {
+      Coproduct::Inl(_) => 0,
+      Coproduct::Inr(tail) => 1 + tail.index(),
     }
-}
-
-impl<
-        'same_process,
-        SERIALIZER: Serializer,
-        CASE_THIS: ParamReprList<'same_process, SERIALIZER>,
-        CASE_OTHER: SplitterOutputRepr<'same_process>,
-    > SplitterOutputRepr<'same_process> for Coproduct<CASE_THIS, CASE_OTHER>
-{
-    fn get_param_ids(self) -> Vec<ParamId> {
-        match self {
-            Coproduct::Inl(inl) => inl.get_param_ids(),
-            Coproduct::Inr(inr) => inr.get_param_ids(),
-        }
-    }
-
-    fn index(self) -> usize {
-        match self {
-            Coproduct::Inl(_) => 0,
-            Coproduct::Inr(tail) => 1 + tail.index(),
-        }
-    }
+  }
 }
 
 /// At least two cases, I guess
 #[async_trait]
-pub trait TypeSplitterBrickHandler<
-    'same_process,
-    SPLITTER_OUTPUT_REPR: SplitterOutputRepr<'same_process>,
->
-{
-    async fn handle(&self, input: InputParams) -> anyhow::Result<SPLITTER_OUTPUT_REPR>;
+pub trait TypeSplitterBrickHandler<'same_process, SPLITTER_OUTPUT_REPR: SplitterOutputRepr<'same_process>> {
+  async fn handle(&self, input: InputParams) -> anyhow::Result<SPLITTER_OUTPUT_REPR>;
 }
 
 // #[async_trait]
@@ -188,21 +143,21 @@ pub trait TypeSplitterBrickHandler<
 
 /// We can add a list of completed actions later
 pub struct SplitterBrick<
-    'same_process,
-    CASE_THIS: ParamReprList<'same_process>,
-    CASE_OTHER: SplitterOutputRepr<'same_process>,
+  'same_process,
+  CASE_THIS: ParamReprList<'same_process>,
+  CASE_OTHER: SplitterOutputRepr<'same_process>,
 > {
-    pub name: String,
-    pub produces: Coproduct<CASE_THIS, CASE_OTHER>,
-    pub handler: Box<dyn TypeSplitterBrickHandler<'same_process, CASE_THIS, CASE_OTHER>>,
+  pub name: String,
+  pub produces: Coproduct<CASE_THIS, CASE_OTHER>,
+  pub handler: Box<dyn TypeSplitterBrickHandler<'same_process, CASE_THIS, CASE_OTHER>>,
 }
 
 #[async_trait]
 pub trait TypeFinalBrickHandler<'same_process, CONSUMES: ParamReprList<'same_process>> {
-    async fn handle(&self, input: CONSUMES::VALUE) -> anyhow::Result<Message>;
+  async fn handle(&self, input: CONSUMES::VALUE) -> anyhow::Result<Message>;
 }
 
 pub struct FinalBrick<'same_process, CONSUMES: ParamReprList<'same_process>> {
-    pub name: String,
-    pub handler: Box<dyn TypeFinalBrickHandler<'same_process, CONSUMES>>,
+  pub name: String,
+  pub handler: Box<dyn TypeFinalBrickHandler<'same_process, CONSUMES>>,
 }
