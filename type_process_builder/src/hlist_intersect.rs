e9294576f75834ec4d1481d246a51bb74a4a8c53
Tomasz Zieliński <zielinski.x360@gmail.com>
2025-08-24 19:55:14 +0200

    qs

--- type_process_builder/src/hlist_intersect.rs
+++ type_process_builder/src/hlist_intersect.rs
@@ -46,7 +46,7 @@ where
 
 #[cfg(test)]
 mod tests {
-  use crate::hlist_intersect::SelectOrAbsent;
+  use crate::hlist_intersect::*;
   use frunk_core::hlist;
   use frunk_core::hlist::{HCons, HNil};
 
@@ -54,9 +54,9 @@ mod tests {
   fn test_select_or_absent() {
     let mut my_list = hlist![42u32, "hello", true];
 
-    // // Example 1: existing type
-    // let val: Option<&u32> = <HCons<u32, HCons<&str, HCons<bool, HNil>>>>::select(&my_list);
-    // println!("Found u32? {:?}", val); // Some(42)
+    // Example 1: existing type
+    let val: Option<&u32> = <HCons<u32, HCons<&str, HCons<bool, HNil>>>>::select(&my_list);
+    println!("Found u32? {:?}", val); // Some(42)
 
     // Example 2: mut reference
     if let Some(val_mut) = <HCons<u32, HCons<&str, HCons<bool, HNil>>>>::select_mut(&mut my_list) {
@@ -66,12 +66,13 @@ mod tests {
 
     // Example 3: type not in HList (Missing)
     let missing: Option<&f64> = my_list.select();
-    println!("Found f64? {:?}", missing); // None
-                                          // let a = hlist![1u8, "hello", true];
-                                          // let b = hlist![false, 1u8, 3.14f32];
-                                          //
-                                          // let intersection = a.intersect(b);
-                                          //
-                                          // assert_eq!(intersection, hlist![1u8, true]);
+    println!("Found f64? {:?}", missing);
+    // None
+    // let a = hlist![1u8, "hello", true];
+    // let b = hlist![false, 1u8, 3.14f32];
+    //
+    // let intersection = a.intersect(b);
+    //
+    // assert_eq!(intersection, hlist![1u8, true]);
   }
 }
