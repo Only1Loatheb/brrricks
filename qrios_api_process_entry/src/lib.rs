90de1db46708c4bb28199d9ce28a403dbce3e59e
Tomasz Zieliński <zielinski.x360@gmail.com>
2026-02-15 21:40:03 +0100

    qs

---
+++ qrios_api_process_entry/src/lib.rs
@@ -0,0 +1,68 @@
+use anyhow::anyhow;
+use frunk_core::{HList, hlist};
+use serde::{Deserialize, Serialize};
+use serde_value::Value;
+use std::collections::HashMap;
+use std::ops::Not;
+use type_process_builder::param_list::ParamValue;
+use type_process_builder::step::Entry;
+use typenum::U0;
+
+#[derive(PartialEq, Debug, Eq, Clone, Copy, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct Msisdn(u64);
+impl Msisdn {
+  pub fn from_string(string: String) -> Option<Msisdn> {
+    string
+      .split_at_checked(string.len().checked_sub(10)?)
+      .and_then(|(_prefix, suffix)| {
+        // deny optional '+' https://doc.rust-lang.org/std/primitive.u64.html#method.from_str
+        let _: () = suffix.starts_with('+').not().then_some(())?;
+        suffix.parse::<u64>().ok()
+      })
+      .map(|x| Msisdn(x))
+  }
+}
+
+#[derive(PartialEq, Debug, Eq, Clone, Copy, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub enum Operator {
+  mtn,
+  airtel,
+  glo,
+  etisalat,
+}
+
+#[derive(PartialEq, Debug, Eq, Clone, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct ShortcodeString(String);
+
+#[derive(PartialEq, Debug, Eq, Clone, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct DialedSessionEntryParam(Msisdn, Operator, ShortcodeString);
+impl ParamValue for DialedSessionEntryParam {
+  type UID = U0;
+}
+
+pub struct DialedSessionEntry;
+impl Entry<Value> for DialedSessionEntry {
+  type Produces = HList![DialedSessionEntryParam];
+
+  async fn handle(
+    &self,
+    mut consumes: HashMap<u64, Value>,
+    shortcode_string: String,
+  ) -> anyhow::Result<Self::Produces> {
+    let msisdn_value = consumes
+      .remove(&0)
+      .ok_or_else(|| anyhow!("Admin error or error on frontend."))?;
+    let msisdn = match msisdn_value {
+      Value::String(string) => Msisdn::from_string(string).ok_or_else(|| anyhow!("Admin error on frontend.")),
+      _ => Err(anyhow!("Admin error on frontend.")),
+    }?;
+    let operator = consumes
+      .remove(&1)
+      .ok_or_else(|| anyhow!("Admin error or error on frontend."))?;
+    Ok(hlist!(DialedSessionEntryParam(
+      msisdn,
+      Operator::deserialize(operator)?,
+      ShortcodeString(shortcode_string)
+    )))
+  }
+}
--- qrios_api_process_runner/src/lib.rs
+++ qrios_api_process_runner/src/lib.rs
@@ -6,17 +6,19 @@ use qrios_api_axum_server::apis::developers_app_endpoints::{
   PostUssdsessioneventAbortResponse, PostUssdsessioneventCloseResponse, PostUssdsessioneventContinueResponse,
   PostUssdsessioneventNewResponse,
 };
+use qrios_api_axum_server::models;
+use qrios_api_axum_server::models::UssdAction::UssdActionOneOf2;
 use qrios_api_axum_server::models::{
   AbortSession, CloseSession, ContinueSession, PostUssdsessioneventAbortHeaderParams,
   PostUssdsessioneventCloseHeaderParams, PostUssdsessioneventContinueHeaderParams, PostUssdsessioneventNewHeaderParams,
-  UssdSessionEventNewSession, UssdSessionEventNewSessionSessionInput,
+  ShowView, UssdSessionCommand, UssdSessionEventNewSession, UssdSessionEventNewSessionSessionInput, UssdView,
+  UssdViewInfoView, UssdViewInputView,
 };
-use qrios_api_types::{EntryParam, Msisdn, Operator, ShortcodeString};
 use serde::{Deserialize, Serialize};
 use serde_value::Value;
 use std::collections::HashMap;
 use std::ops::Not;
-use type_process_builder::builder::{FinalizedProcess, PreviousRunYieldedAt, RunnableProcess};
+use type_process_builder::builder::{FinalizedProcess, PreviousRunYieldedAt, RunOutcome, RunnableProcess};
 use type_process_builder::step::FailedInputValidationAttempts;
 
 pub struct QriosUssdApiService<Process: FinalizedProcess> {
@@ -72,19 +74,54 @@ impl<Process: FinalizedProcess + Sync> qrios_api_axum_server::apis::developers_a
     header_params: &PostUssdsessioneventNewHeaderParams,
     body: &UssdSessionEventNewSession,
   ) -> Result<PostUssdsessioneventNewResponse, ()> {
-    let shortcode_string = match body.input {
+    let shortcode_string = match body.input.clone() {
       UssdSessionEventNewSessionSessionInput::UssdSessionEventNewSessionSessionInputOneOf(x) => x.dial.shortcode_string,
       UssdSessionEventNewSessionSessionInput::UssdSessionEventNewSessionSessionInputOneOf1(_) => todo!(),
       UssdSessionEventNewSessionSessionInput::UssdSessionEventNewSessionSessionInputOneOf2(_) => todo!(),
     };
-    self
+    let a = self
       .process
       .resume_run(
-        HashMap::from([(0, Value::String(body.msisdn)), (1, Value::String(body.operator))]),
+        HashMap::from([
+          (0, Value::String(body.msisdn.clone())),
+          (1, Value::String(body.operator.clone())),
+        ]),
         PreviousRunYieldedAt(0),
         shortcode_string,
         FailedInputValidationAttempts(0),
       )
-      .await?
+      .await;
+    match a {
+      // fixme session store the stuff
+      Ok(RunOutcome::Yield(message, session_context, current_run_yielded_at)) => {
+        Ok(UssdView::UssdViewInputView(UssdViewInputView {
+          message: message.0,
+          r_type: "InputView".into(),
+        }))
+      }
+      Ok(RunOutcome::RetryUserInput(message)) => Ok(UssdView::UssdViewInputView(UssdViewInputView {
+        message: message.0,
+        r_type: "InputView".into(),
+      })),
+      Ok(RunOutcome::Finish(message)) => Ok(UssdView::UssdViewInfoView(UssdViewInfoView {
+        message: message.0,
+        r_type: "InfoView".into(),
+      })),
+      Err(_) => Err(()),
+    }
+    .map(|ussd_view| {
+      PostUssdsessioneventNewResponse::Status200_SessionStartHasBeenSuccessfullyHandledByTheDeveloper(
+        UssdSessionCommand {
+          action: UssdActionOneOf2(models::UssdActionOneOf2 {
+            show_view: ShowView {
+              r_type: "ShowView".into(),
+              view: ussd_view,
+            },
+          }),
+          context_data: "session store magic number goes here".into(),
+          session_tag: None,
+        },
+      )
+    })
   }
 }
