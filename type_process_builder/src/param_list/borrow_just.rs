f3dfae267de8257e372a7f26d9209cdb3f3274b4
Tomasz Zieliński <zielinski.x360@gmail.com>
2026-03-15 07:12:59 +0100

    qs

--- type_process_builder/src/param_list/borrow_just.rs
+++ type_process_builder/src/param_list/borrow_just.rs
@@ -2,25 +2,24 @@ use frunk_core::hlist::{HCons, HNil, Plucker};
 use frunk_core::traits::ToRef;
 
 pub trait BorrowJust<'a, Target: ToRef<'a>, Indices> {
-  fn borrow_just(&'a self) -> <Target as ToRef<'a>>::Output;
+  fn borrow_just(self) -> <Target as ToRef<'a>>::Output;
 }
 
-impl<'a, Source> BorrowJust<'a, HNil, HNil> for Source {
+impl<'a, Source, Any> BorrowJust<'a, HNil, Any> for &'a Source {
   #[inline(always)]
-  fn borrow_just(&'a self) -> HNil {
+  fn borrow_just(self) -> HNil {
     HNil
   }
 }
 
 impl<'a, TargetHead: 'a, TargetTail: ToRef<'a>, SourceHead: 'a, SourceTail: 'a, IndexHead: 'a, IndexTail>
-BorrowJust<'a, HCons<TargetHead, TargetTail>, HCons<IndexHead, IndexTail>> for HCons<SourceHead, SourceTail>
+BorrowJust<'a, HCons<TargetHead, TargetTail>, HCons<IndexHead, IndexTail>> for &'a HCons<SourceHead, SourceTail>
 where
   &'a HCons<SourceHead, SourceTail>: Plucker<&'a TargetHead, IndexHead>,
-  <&'a HCons<SourceHead, SourceTail> as Plucker<&'a TargetHead, IndexHead>>::Remainder:
-  BorrowJust<'a, TargetTail, IndexTail>,
+  <&'a HCons<SourceHead, SourceTail> as Plucker<&'a TargetHead, IndexHead>>::Remainder: BorrowJust<'a, TargetTail, IndexTail>,
 {
   #[inline(always)]
-  fn borrow_just(&'a self) -> <HCons<TargetHead, TargetTail> as ToRef<'a>>::Output {
+  fn borrow_just(self) -> <HCons<TargetHead, TargetTail> as ToRef<'a>>::Output {
     let (head, remainder) = self.pluck();
     HCons { head, tail: remainder.borrow_just() }
   }
