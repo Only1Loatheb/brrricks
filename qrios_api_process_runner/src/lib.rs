52aa0af648f8d8569d1e5e272113150554d038ec
Tomasz Zieliński <zielinski.x360@gmail.com>
2026-03-15 12:15:53 +0100

    qs

--- qrios_api_process_runner/src/lib.rs
+++ qrios_api_process_runner/src/lib.rs
@@ -24,15 +24,15 @@ use type_process_builder::builder::{
 };
 use type_process_builder::step::FailedInputValidationAttempts;
 
-pub struct QriosUssdApiService<'a, Process: FinalizedProcess> {
+pub struct QriosUssdApiService<Process: FinalizedProcess> {
   process: RunnableProcess<Process>,
-  pool: &'a PgPool,
+  pool: PgPool,
   ordered_all_unique_param_uids: Vec<ParamUID>,
   get_session_context_query: GetSessionContextQuery,
 }
 
-impl<'a, Process: FinalizedProcess> QriosUssdApiService<'a, Process> {
-  pub async fn new(process: RunnableProcess<Process>, pool: &'a PgPool) -> Result<Self, sqlx::Error> {
+impl<Process: FinalizedProcess> QriosUssdApiService<Process> {
+  pub async fn new(process: RunnableProcess<Process>, pool: PgPool) -> Result<Self, sqlx::Error> {
     let ordered_all_unique_param_uids = process.ordered_all_unique_param_uids();
     create_session_context_table(&pool, &process, &ordered_all_unique_param_uids).await?;
     let get_session_context_query = build_get_session_context_query(&process, &ordered_all_unique_param_uids);
@@ -40,12 +40,12 @@ impl<'a, Process: FinalizedProcess> QriosUssdApiService<'a, Process> {
   }
 }
 
-impl<Process: FinalizedProcess> ErrorHandler<()> for QriosUssdApiService<'_, Process> {}
+impl<Process: FinalizedProcess> ErrorHandler<()> for QriosUssdApiService<Process> {}
 
 #[allow(unused_variables)]
 #[async_trait]
 impl<Process: FinalizedProcess + Sync> qrios_api_axum_server::apis::developers_app_endpoints::DevelopersAppEndpoints
-for QriosUssdApiService<'_, Process>
+for QriosUssdApiService<Process>
 {
   /// I guess we could delete by [AbortSession] session_id
   async fn post_ussdsessionevent_abort(
@@ -68,7 +68,7 @@ for QriosUssdApiService<'_, Process>
     body: &CloseSession,
   ) -> Result<PostUssdsessioneventCloseResponse, ()> {
     let session_id = body.context_data.parse::<i64>().map_err(|_| ())?;
-    delete_session_context(self.pool, &self.process, session_id).await.map_err(|_| ())?;
+    delete_session_context(&self.pool, &self.process, session_id).await.map_err(|_| ())?;
     Ok(PostUssdsessioneventCloseResponse::Status200_SessionEndHasBeenSuccessfullyHandledByTheDeveloper)
   }
 
@@ -88,7 +88,7 @@ for QriosUssdApiService<'_, Process>
     };
     let session_id = body.context_data.parse::<i64>().map_err(|_| ())?;
     let (previous_run_yielded_at, failed_input_validation_attempts, session_context) =
-      get_session_context(self.pool, &self.get_session_context_query, session_id, &self.ordered_all_unique_param_uids)
+      get_session_context(&self.pool, &self.get_session_context_query, session_id, &self.ordered_all_unique_param_uids)
         .await
         .map_err(|_| ())?;
     let already_stored_params = session_context.iter().map(|x| x.0).collect::<HashSet<_>>();
@@ -113,15 +113,15 @@ for QriosUssdApiService<'_, Process>
         Ok(UssdView::UssdViewInputView(UssdViewInputView { message: message.0, r_type: "InputView".into() }))
       }
       Ok(RunOutcome::RetryUserInput(message)) => {
-        increment_failed_input_validation_attempts(self.pool, &self.process, session_id).await.map_err(|_| ())?;
+        increment_failed_input_validation_attempts(&self.pool, &self.process, session_id).await.map_err(|_| ())?;
         Ok(UssdView::UssdViewInputView(UssdViewInputView { message: message.0, r_type: "InputView".into() }))
       }
       Ok(RunOutcome::Finish(message)) => {
-        delete_session_context(self.pool, &self.process, session_id).await.map_err(|_| ())?;
+        delete_session_context(&self.pool, &self.process, session_id).await.map_err(|_| ())?;
         Ok(UssdView::UssdViewInfoView(UssdViewInfoView { message: message.0, r_type: "InfoView".into() }))
       }
       Err(_) => {
-        delete_session_context(self.pool, &self.process, session_id).await.map_err(|_| ())?;
+        delete_session_context(&self.pool, &self.process, session_id).await.map_err(|_| ())?;
         Err(())
       }
     }
@@ -201,24 +201,41 @@ mod tests {
   use crate::QriosUssdApiService;
   use frunk_core::hlist::HNil;
   use qrios_api_process_entry::DialedSessionEntry;
+  use sqlx::postgres::PgPoolOptions;
+  use std::sync::Arc;
+  use tokio::net::TcpListener;
+  use tokio::signal;
   use type_process_builder::builder::FinalizedProcess;
   use type_process_builder::builder::{FlowingProcess, Message};
   use type_process_builder::step::Final;
 
   #[tokio::test]
-  async fn test_split() {
+  async fn no_op_process_test() {
     struct NoOpFinalStep;
     impl Final for NoOpFinalStep {
       type Consumes = HNil;
-      async fn handle(&self, consumes: Self::Consumes) -> anyhow::Result<Message> {
+      async fn handle(&self, _consumes: Self::Consumes) -> anyhow::Result<Message> {
         Ok(Message("Good bye".into()))
       }
     }
 
-    let process = DialedSessionEntry.end(NoOpFinalStep).build("no_op_test", 0);
+    let process = DialedSessionEntry.end(NoOpFinalStep).build("no_op_process", 0);
 
-    let service = QriosUssdApiService::new(process).await.expect("Failed to create QriosUssdApiService");
+    let pool = PgPoolOptions::new()
+      .max_connections(5)
+      .connect("postgres://postgres:password@localhost/test").await.expect("Failed to connect to PostgreSQL server");
 
+    let service = QriosUssdApiService::new(process, pool).await.expect("Failed to create QriosUssdApiService");
+    tracing_subscriber::fmt::init();
+    let app = qrios_api_axum_server::server::new(Arc::new(service));
+    // Add layers to the router
+    // let app = app.layer(...);
+    let listener = TcpListener::bind("127.0.0.1:0").await.expect("Failed to bind random port");
+    let addr = listener.local_addr().expect("Failed to get server local address");
+    axum::serve(listener, app)
+      .with_graceful_shutdown(shutdown_signal())
+      .await
+      .expect("Failed to start server");
     // let run_result = process
     //   .resume_run(
     //     session_init_value(),
@@ -228,5 +245,29 @@ mod tests {
     //   )
     //   .await;
     // assert_eq!(run_result.unwrap(), RunOutcome::Finish(Message("Good bye".into())));
+  }
+
+  async fn shutdown_signal() {
+    let ctrl_c = async {
+      signal::ctrl_c()
+        .await
+        .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+      signal::unix::signal(signal::unix::SignalKind::terminate())
+        .expect("failed to install signal handler")
+        .recv()
+        .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
   }
 }
