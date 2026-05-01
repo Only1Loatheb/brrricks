6c80dc9d952aa34e507790efc21e3ef03f597547
Tomasz Zieliński <zielinski.x360@gmail.com>
2026-05-01 08:08:23 +0200

    Revert adding HList union.
    This reverts commit 2984166ed11a606859fce999df8751256801de4b.

--- type_process_builder/src/param_list/intersect.rs
+++ type_process_builder/src/param_list/intersect.rs
@@ -1,8 +1,54 @@
-use crate::{
-  builder::{contains::Contains, filter::Filter},
-  param_list::ParamValue,
-};
+use crate::param_list::ParamValue;
 use frunk_core::hlist::{HCons, HNil};
+use std::ops::BitOr;
+use typenum::{B0, B1, Bit, IsEqual};
+
+////////// Contains //////////
+
+pub trait Contains<Needle: ParamValue> {
+  type IsContained: Bit;
+}
+
+impl<Needle: ParamValue> Contains<Needle> for HNil {
+  type IsContained = B0;
+}
+
+impl<Needle: ParamValue, Head: ParamValue, Tail: Contains<Needle>> Contains<Needle> for HCons<Head, Tail>
+where
+  Needle::UID: IsEqual<Head::UID>,
+  <Needle::UID as IsEqual<Head::UID>>::Output: BitOr<<Tail as Contains<Needle>>::IsContained>,
+  <<Needle::UID as IsEqual<Head::UID>>::Output as BitOr<<Tail as Contains<Needle>>::IsContained>>::Output: Bit,
+{
+  type IsContained =
+    <<Needle::UID as IsEqual<Head::UID>>::Output as BitOr<<Tail as Contains<Needle>>::IsContained>>::Output;
+}
+
+////////// Filter //////////
+
+pub trait Filter<Head, Tail> {
+  type Filtered;
+  fn filter(head: Head, tail: Tail) -> Self::Filtered;
+}
+
+impl<Head, Tail> Filter<Head, Tail> for B1 {
+  type Filtered = HCons<Head, Tail>;
+
+  #[inline(always)]
+  fn filter(head: Head, tail: Tail) -> Self::Filtered {
+    HCons { head, tail }
+  }
+}
+
+impl<Head, Tail> Filter<Head, Tail> for B0 {
+  type Filtered = Tail;
+
+  #[inline(always)]
+  fn filter(_head: Head, tail: Tail) -> Self::Filtered {
+    tail
+  }
+}
+
+////////// Intersection //////////
 
 pub trait Intersect<RHS> {
   type Intersection;
